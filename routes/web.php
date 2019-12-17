dfdc35d347ef5eaef576aa1bee358cb2d5202d5f
ystmrdk <rattyasta1@gmail.com>
2019-12-17 11:35:21 +0700

    added auth

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,19 @@ Route::get('/', function () {
     return view('base');
 });
 Auth::routes();
-Route::get('/jobs', 'JobsController@index');
-Route::get('/jobs/tambah', 'JobsController@create')->name('tambah_jobs');
-Route::get('/jobs/tambah/store', 'JobsController@store')->name('storejobs');
-Route::get('/jobs/tambah/edit', 'JobsController@update')->name('editjobs');
-Route::get('/jobs/tambah/delete', 'JobsController@destroy')->name('deletejobs');
-Route::get('/employees', 'EmployeesController@index');
-Route::get('/employees/tambah', 'EmployeesController@create')->name('tambahemployees');
-Route::get('/employees/tambah/store', 'EmployeesController@store')->name('storeemployees');
-Route::get('/employees/tambah/edit', 'EmployeesController@update')->name('editemployees');
-Route::get('/employees/tambah/delete', 'EmployeesController@destroy')->name('deleteemployees');
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/info', function () {
+        return view('info');
+    });
+    Route::get('/jobs', 'JobsController@index');
+    Route::get('/jobs/tambah', 'JobsController@create')->name('tambah_jobs');
+    Route::post('/jobs/tambah/store', 'JobsController@store')->name('storejobs');
+    Route::get('/jobs/tambah/edit/{id}', 'JobsController@edit')->name('editjobs');
+    Route::get('/jobs/tambah/delete/{id}', 'JobsController@destroy')->name('deletejobs');
+    Route::get('/employees', 'EmployeesController@index');
+    Route::get('/employees/tambah', 'EmployeesController@create')->name('tambahemployees');
+    Route::post('/employees/tambah/store', 'EmployeesController@store')->name('storeemployees');
+    Route::get('/employees/tambah/edit/{id}', 'EmployeesController@edit')->name('editemployees');
+    Route::get('/employees/tambah/delete', 'EmployeesController@destroy')->name('deleteemployees');
+    Route::get('/home', 'HomeController@index')->name('home');
+});
